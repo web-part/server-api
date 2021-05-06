4c216bd9224cf7e7708c8cb393a9011a47b5e2aa
micty <mictydeng@qq.com>
2021-05-06 17:26:09 +0800

    1.0.5

--- index.js
+++ index.js
@@ -15,45 +15,59 @@ const config = require('./config'); //全局的配置文件。
 
 
 
+
 module.exports = {
 
+  
+    /**
+    * 启动 API 接口服务。
+    * @param {app} app 
+    * @param {object} opt 
+    *   opt = {
+    *       url: 'http://localhost:3001',
+    *       path: 'api',
+    *       allowCrossOrigin: true,
+    *       stat: {},
+    *   };
+    */
     start(app, opt) {
         let { bodyParser, } = config;
-        let { server, stat, } = opt;
-
+        let { stat, } = opt;
 
 
         app.use(CookieParser());
         app.use(BodyParser.json(bodyParser.json));
         app.use(BodyParser.urlencoded(bodyParser.urlencoded));
 
-         //使用 gzip 压缩。 这个跟 sse 冲突。
+        //使用 gzip 压缩。 这个跟 sse 冲突。
         app.use(Compression({
             filter: function shouldCompress(req, res) {
                 //这个接口不要压缩，否则不会自动发送消息给客户端。
                 if (req.url.startsWith('/api/sse/')) {
-                    // don't compress responses with this request header
+                    // don't compress responses with this request header.
                     return false;
                 }
 
-                // fallback to standard filter function
+                // fallback to standard filter function.
                 return Compression.filter(req, res);
             }
         }));
 
         
 
+        if (opt.allowCrossOrigin) {
+            app.use(function (req, res, next) {
+                res.set({ 'Access-Control-Allow-Origin': '*', });
+                next();
+            });
+        }
 
-        app.use(function (req, res, next) {
-            res.set({ 'Access-Control-Allow-Origin': '*', });
-            next();
-        });
 
 
 
         FileList.config({
             'root': stat.htdocs,
-            'url': server.networkUrl,
+            'url': opt.url,
         });
 
         MD5.config({
@@ -66,36 +80,34 @@ module.exports = {
 
         Router.use(app, {
             module: 'FileList',
-            base: '/api/FileList.',
+            base: `/${opt.path}/FileList.`,
             get: ['get', 'read',],
         });
 
         Router.use(app, {
             module: 'Stat',
-            base: '/api/Stat.',
+            base: `/${opt.path}/Stat.`,
             get: ['get',],
         });
 
         Router.use(app, {
             module: 'MD5',
-            base: '/api/MD5.',
+            base: `/${opt.path}/MD5.`,
             get: ['get',],
         });
 
         Router.use(app, {
             module: 'Log',
-            base: '/api/Log.',
+            base: `/${opt.path}/Log.`,
             get: ['get', 'clear',],
         });
 
-
-        app.get('/api/sse/Terminal.exec', SSEExpress(), function (req, res, next) {
+        app.get(`/${opt.path}/sse/Terminal.exec`, SSEExpress(), function (req, res, next) {
             Terminal.exec(req, res);
         });
 
-        
-
-        
+        //返回接口的基 url。
+        return `${opt.url}/${opt.path}/`;
 
     },
 };
