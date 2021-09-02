490c7ff161bb48276096c3940d1a6febd8bcf559
micty <mictydeng@qq.com>
2021-09-02 16:36:23 +0800

    windows encoding

--- modules/Terminal/Sender/List.js
+++ modules/Terminal/Sender/List.js
@@ -6,7 +6,29 @@
 //因此可以先收集起来，等到结尾标志出现再串联起来成一条完整的消息。
 
 const iconv = require('iconv-lite');
+const Encoding = require('../../../lib/Encoding');
 
+function parse(data, isEnd) {
+    let isWin32 = process.platform == 'win32';
+    let msg = data.toString();
+
+    //windows 平台的，需要猜测编码类型并且进行解码。
+    if (isWin32) {
+        let encoding = Encoding.isUTF8(msg) ? 'utf8' : 'cp936';
+        msg = iconv.decode(data, encoding);
+    }
+
+    //未指定，则从内容中判断。
+    if (isEnd === undefined) {
+        isEnd = isWin32 ? msg.endsWith('\n') : msg.endsWith('\n') && !msg.endsWith('\r\n');
+    }
+    else if (isEnd) { //参数明确指定了要结束。
+        msg += '\n';
+    }
+
+    return { msg, isEnd, isWin32, };
+
+}
 
 
 
@@ -16,21 +38,9 @@ class List {
         this.list = [];
     }
 
-    push(data, isEnd) {
-        let isWin32 = process.platform == 'win32';
-        let msg = isWin32 ? iconv.decode(data, 'cp936') : data.toString();
-    
-        //未指定，则从内容中判断。
-        if (isEnd === undefined) {
-            isEnd = isWin32 ? msg.endsWith('\n') : msg.endsWith('\n') && !msg.endsWith('\r\n');
-        }
-        else if (isEnd) { //参数明确指定了要结束。
-            msg += '\n';
-        }
-
-
+    push(data, needEnd) {
+        let { msg, isEnd, } = parse(data, needEnd);
         let { list, } = this;
-
 
         list.push(msg);
 
